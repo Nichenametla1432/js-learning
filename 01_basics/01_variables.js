74d2218b84cdddb4f4715c84be8248064843acc8
Nichenametla Sri venkat <154978698+Nichenametla1432@users.noreply.github.com>
2026-06-04 08:59:57 +0000

    commit config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const  accountId = 144553;
+let accountEmail = "venkat@google.com";
+var accountPassword = "1234"; 
+accountCity = "Yerraguntla";
+
+// accountId = 1234; // not allowed 
+
+accountEmail = "sv@goole.com";
+accountPassword = "2211221"
+accountCity = "banglore"
+/*
+prefer not to use 
+because  of issues in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountEmail,accountPassword, accountCity])
