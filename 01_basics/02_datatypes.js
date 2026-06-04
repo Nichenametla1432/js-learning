e6dfb54a2a761a335a91e24d851d06c33172916c
Nichenametla Sri venkat <154978698+Nichenametla1432@users.noreply.github.com>
2026-06-04 09:56:04 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict" // treat all JS code as newer version
+
+//alert(3 + 3) // we are using node.js, not browser
+
+//console.log(3 + 3) console.log("sri venkat") // also raises an error because js dont know where statement end so need use ; or wite in new line
+
+console.log(3 + 3); console.log("sri venkat")
+
+//or 
+console.log(2 + 3);
+
+console.log("sri venkat")
+
+console.log(3 
+    +
+     3) //also correct and code readability should  be high
+
+//data types 
+let name = "N.Sri Venkat"; 
+let age = 22;     
+let isLoggedIn = true;
+
+//primitive data types
+// Number => 2to the power of 53 (range)
+// bigint =>larger than number
+// string => " "
+// boolean => true / false
+// null => standlone value
+// undefined => variable declared but no value is assigned 
+// symbol => for case of uniqueness
+
+// __*__
+
+// object =>dicuss in furtherly
+
+console.log(typeof "sriVEnkat"); // typeof is defines of what kind of data type is it? 
+console.log(typeof null); // o/p: object (interview question)
+
